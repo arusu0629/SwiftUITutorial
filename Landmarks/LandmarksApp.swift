dd6a1907b3dfaabd6d013940630df4dcad0babe2
af_nakandakari_toru <nakandakari_toru@andfactory.co.jp>
2023-03-28 22:54:04 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by af_nakandakari_toru on 2023/03/28.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
