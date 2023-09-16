d1fd6eb0a5e89c0ef2d6372fa01821de3c31813e
t-nakandakari <arusu0629@gmail.com>
2023-09-16 19:14:43 +0900

    [Section1] Add a Category View

---
+++ Landmarks/Landmarks/Views/Categories/CategoryHome.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by t-nakandakari on 2023/09/16.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationView {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
