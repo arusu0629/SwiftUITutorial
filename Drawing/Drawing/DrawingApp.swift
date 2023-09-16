0516c8548aed2dff32f06076b542cfe112b7db36
t-nakandakari <arusu0629@gmail.com>
2023-09-16 15:58:09 +0900

    [Drawing Paths and Shapes] first commit

---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by t-nakandakari on 2023/09/16.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
