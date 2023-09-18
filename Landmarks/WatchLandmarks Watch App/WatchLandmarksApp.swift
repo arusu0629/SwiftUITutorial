deace39ace0bb5eddd19814331538a8c19837805
t-nakandakari <arusu0629@gmail.com>
2023-09-18 17:32:58 +0900

    [Section1] Add a watchOS Target

---
+++ Landmarks/WatchLandmarks Watch App/WatchLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchLandmarksApp.swift
+//  WatchLandmarks Watch App
+//
+//  Created by t-nakandakari on 2023/09/18.
+//
+
+import SwiftUI
+
+@main
+struct WatchLandmarks_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
