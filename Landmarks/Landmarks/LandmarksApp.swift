ef2b24631fc4f68837d950521ca5c07551eadbf9
t-nakandakari <arusu0629@gmail.com>
2023-09-18 19:17:20 +0900

    [Section5] Create a Custom Notification Interface

--- Landmarks/Landmarks/LandmarksApp.swift
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -6,15 +6,29 @@
 //
 
 import SwiftUI
+import UserNotifications
 
 @main
 struct LandmarksApp: App {
     @StateObject private var modelData = ModelData()
+    let center = UNUserNotificationCenter.current()
+    
+    init() {
+        center.requestAuthorization(options: [.alert, .sound, .badge], completionHandler: { (granted, error) in
+            if let error: Error {
+                print(error)
+            }
+        })
+    }
     
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(modelData)
         }
+        
+        #if os(watchOS)
+        WKNotificationScene(controller: NOtificationController.self, category: "LandmarkNear")
+        #endif
     }
 }
